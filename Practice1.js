7aabfd12a7ed23af27b3640811776c11ecc6529f
CodeGawd914 <Travisgdesir31@gmail.com>
2019-02-20 09:35:43 -0500

    solved in an hr took way 2 long

---
+++ Practice1.js
@@ -0,0 +1,16 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
+var twoSum = function(nums, target) {
+  let holder = []
+    for (i = 0; i< nums.length;i++){
+      for (j = i +1; j< nums.length; j++){
+        if (nums[j] == target - nums[i]){
+          return [i,j]
+        }
+      }
+    }
+};
